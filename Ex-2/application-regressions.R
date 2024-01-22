d06ee838feeb11994b1e6bd21c9066ea63d49747
aoluwolerotimi <a.oluwolerotimi@gmail.com>
2024-01-22 12:29:09 -0500

    Using arrow pakage

--- Ex-2/application-regressions.R
+++ Ex-2/application-regressions.R
@@ -1,1 +1,9 @@
 # EX 2 
+
+# install.packages("arrow")
+library(arrow)
+
+data_path <- "/Users/aoluwolerotimi/Datasets/"
+applications <- read_feather(paste0(data_path,"app_data_starter.feather"))
+
+View(applications)

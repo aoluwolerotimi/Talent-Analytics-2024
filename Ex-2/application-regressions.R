c8c6580d319a720cfa7614d70c4dbf2f6021abca
aoluwolerotimi <a.oluwolerotimi@gmail.com>
2024-01-22 22:30:36 -0500

    Finally running the pre-req code

--- Ex-2/application-regressions.R
+++ Ex-2/application-regressions.R
@@ -1,9 +1,130 @@
 # EX 2 
 
-# install.packages("arrow")
+## LOADING DATA
+
 library(arrow)
+library(tidyverse)
+library(lubridate)
+
 
 data_path <- "/Users/aoluwolerotimi/Datasets/"
 applications <- read_feather(paste0(data_path,"app_data_starter.feather"))
 
 View(applications)
+
+## INDIVIDUAL LEVEL VARIABLES: GENDER ## 
+library(gender)
+library(devtools)
+
+# install_genderdata_package() didn't work 
+# install.packages("remotes")
+library(remotes)
+options(timeout=400)
+
+
+# get a list of first names without repetitions
+examiner_names <- applications %>% 
+  distinct(examiner_name_first)
+
+examiner_names
+
+# get a table of names and gender
+examiner_names_gender <- examiner_names %>% 
+  do(results = gender(.$examiner_name_first, method = "ssa")) %>% 
+  unnest(cols = c(results), keep_empty = TRUE) %>% 
+  select(
+    examiner_name_first = name,
+    gender,
+    proportion_female
+  )
+
+examiner_names_gender
+
+# remove extra colums from the gender table
+examiner_names_gender <- examiner_names_gender %>% 
+  select(examiner_name_first, gender)
+
+View(examiner_names_gender)
+
+# joining gender back to the dataset
+applications <- applications %>% 
+  left_join(examiner_names_gender, by = "examiner_name_first")
+
+# cleaning up
+rm(examiner_names)
+rm(examiner_names_gender)
+gc()
+
+
+
+## INDIVIDUAL LEVEL VARIABLES: RACE ## 
+
+library(wru)
+
+examiner_surnames <- applications %>% 
+  select(surname = examiner_name_last) %>% 
+  distinct()
+
+examiner_surnames
+
+examiner_race <- predict_race(voter.file = examiner_surnames, surname.only = T) %>% 
+  as_tibble()
+
+examiner_race
+
+examiner_race <- examiner_race %>% 
+  mutate(max_race_p = pmax(pred.asi, pred.bla, pred.his, pred.oth, pred.whi)) %>% 
+  mutate(race = case_when(
+    max_race_p == pred.asi ~ "Asian",
+    max_race_p == pred.bla ~ "black",
+    max_race_p == pred.his ~ "Hispanic",
+    max_race_p == pred.oth ~ "other",
+    max_race_p == pred.whi ~ "white",
+    TRUE ~ NA_character_
+  ))
+
+examiner_race
+
+examiner_race <- examiner_race %>% 
+  select(surname,race)
+
+applications <- applications %>% 
+  left_join(examiner_race, by = c("examiner_name_last" = "surname"))
+
+rm(examiner_race)
+rm(examiner_surnames)
+gc()
+
+
+## INDIVIDUAL LEVEL VARIABLES: TENURE ## 
+
+examiner_dates <- applications %>% 
+  select(examiner_id, filing_date, appl_status_date) 
+
+examiner_dates
+
+examiner_dates <- examiner_dates %>% 
+  mutate(start_date = ymd(filing_date), end_date = as_date(dmy_hms(appl_status_date)))
+
+
+examiner_dates <- examiner_dates %>% 
+  group_by(examiner_id) %>% 
+  summarise(
+    earliest_date = min(start_date, na.rm = TRUE), 
+    latest_date = max(end_date, na.rm = TRUE),
+    tenure_days = interval(earliest_date, latest_date) %/% days(1)
+  ) %>% 
+  filter(year(latest_date)<2018)
+
+examiner_dates
+
+applications <- applications %>% 
+  left_join(examiner_dates, by = "examiner_id")
+
+rm(examiner_dates)
+gc()
+
+# will run the below once i get the package working
+# write_feather(applications, paste0(data_path,"app_data_starter_coded.feather"))
+
+
